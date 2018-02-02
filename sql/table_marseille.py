b5bc2260b8d7ffba40d30324bc480db00b4cb955
itsgood2bebad <kevinsirlin@outlook.fr>
2018-02-02 22:15:55 +0100

    add

---
+++ sql/table_marseille.py
@@ -0,0 +1,38 @@
+import sqlite3
+#creation base de donnés en python
+class Mabase():
+	def __init__(self): #creation d'un objet
+		self.conn = sqlite3.connect('mabase.db')
+#création d'une simple base 
+	def creer(self):
+		c = self.conn.cursor()
+#creation de la base avec les valeurs comme sur l'exercice
+		try:
+			c.execute('create table vente (id INTEGER PRIMARY KEY,bookname VARCHAR(50), sales INTEGER)')
+
+			c.execute('insert into vente values (null,"bookC","4")')
+			c.execute('insert into vente values (null,"bookA","3")')
+			c.execute('insert into vente values (null,"bookB","9")')
+			c.execute('insert into vente values (null,"bookC","11")')
+# sauvegarde des modifications
+			self.conn.commit()
+
+			c.close()
+			print("ok table")
+			return True
+
+		except:
+			self.close()
+			return False
+	#affichage du tableau
+	def lire(self):
+
+		c = self.conn.cursor()
+		c.execute("SELECT * FROM vente")
+		for row in c:
+			print (row)
+		c.close()
+#appel des fonctions 
+mabase = Mabase()
+if not mabase.creer():
+	mabase.lire()
